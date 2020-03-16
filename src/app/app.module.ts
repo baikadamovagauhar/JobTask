d270902bf9f7ab0012f1a84d63653222dd3d8031
Gauhar <gauharbaikadamova1999@gmail.com>
2020-03-16 12:13:59 +0300

    relocated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent} from './components/core/header/header.component';
+import { FooterComponent} from './components/core/footer/footer.component';
 import { NoFoundComponent } from './components/no-found/no-found.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { ChosenMoviesComponent } from './components/chosen-movies/chosen-movies.component';
-import { ContentComponent } from './core/content/content.component';
+import { ContentComponent} from './components/core/content/content.component';
 import { FormsModule} from '@angular/forms';
 
 
--- src/app/components/core/content/content.component.ts
+++ src/app/components/core/content/content.component.ts
@@ -1,26 +1,19 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {DataService} from '../../services/data.service';
+import {DataService} from '../../../services/data.service';
 import {Subscription} from 'rxjs';
 import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss'],
-  providers: [DataService]
-
+  styleUrls: ['./content.component.scss']
 })
+
 export class ContentComponent implements OnInit, OnDestroy {
 
   movies: any[];
   searchText: string;
-  filters = [
-    {name: ' Filtered by', value: 0},
-    {name: ' popularity', value: 1},
-    {name: ' rates', value: 2},
-    {name: ' increasing duration', value: 3},
-    {name: ' decreasing duration', value: 4}
-  ];
+
   back = {
       title: "Joker",
       description: "“Joker” is an original, standalone story. Arthur Fleck, a man disregarded by society, is not only a gritty character study, but also a broader cautionary tale.",
@@ -31,20 +24,20 @@ export class ContentComponent implements OnInit, OnDestroy {
       director: 'Todd Phillips',
       starring: 'Joaquin Phoenix, Robert De Niro, Zazie Beetz'
     };
-  genre: any
+  genre: any;
   genres: any = {};
   keys: any;
+
   private  querySubscription: Subscription;
 
   constructor(private dataService: DataService, private route: ActivatedRoute) {
     this.querySubscription = route.queryParams.subscribe(
-      (queryParam: any) =>{
+      (queryParam: any) => {
         this.genre = queryParam['genre'];
         this.getMovies(this.genre);
       }
     )
   }
-
   ngOnInit() {
     this.getMovies(this.genre);
     this.getGenres();
@@ -54,11 +47,9 @@ export class ContentComponent implements OnInit, OnDestroy {
       this.querySubscription.unsubscribe();
     }
   }
-
   getMovies(genre){
     this.movies = this.dataService.getMovies(genre);
   }
-
   getGenres(){
     this.keys = Object.keys(this.dataService.getGenre());
     this.genres = this.dataService.getGenre();
